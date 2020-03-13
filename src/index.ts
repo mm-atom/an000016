0ca77dcc6f29fcb5a2150d25bfb3a4546111c845
taoqf <tao_qf@163.com>
2020-03-13 13:20:39 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
+import { getLogger } from 'log4js';
+import { createClient } from 'redis';
+import config from '@mmstudio/config';
 
-export default function an16(param1: string, param2: string) {
+const logger = getLogger();
+const REDIS = config.redis;
+
+export default function an16(key: string, value: unknown) {
+	const client = open();
+	return new Promise<boolean>((resolve) => {
+		const v = JSON.stringify(value);
+		// EX seconds PX milliseconds NX XX	https://redis.io/commands/set
+		client.set(key, v, 'EX', REDIS.expiration, (error, res) => {
+			if (error) {
+				logger.error(error);
+				client.end(false);
+				resolve(false);
+			} else {
+				client.end(true);
+				resolve(res === 'OK');
+			}
+			client.quit();
+		});
+	});
+}
+
+function open() {
+	const client = createClient(REDIS.url);
+	client.on('error', (e) => {
+		logger.error(e);
+		logger.error('Redis Error thrown, process will exit with code -1');
+		process.exit(-1);
+	});
+	// process.on('beforeExit', () => {
+	// 	client.quit();
+	// });
+	return client;
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,20 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('redis set single', async (t) => {
+	const key = 'key';
+	const value = 'value';
+	const r = await a(key, value);
+	t.is(r, true);
+});
+
+test('redis set multy', async (t) => {
+	const key1 = 'key1';
+	const value1 = 'value1';
+	const r1 = await a(key1, value1);
+	t.is(r1, true);
+	const key2 = 'key2';
+	const value2 = 'value2';
+	const r2 = await a(key2, value2);
+	t.is(r2, true);
 });
